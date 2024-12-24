fc63de43d0d01cd06a4e2411b17451ab3fa96bef
Artemii Veshkin <artemveshkin@lawn.vla.yp-c.yandex.net>
2024-12-24 23:36:45 +0300

    update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-if [ -z "$2" ]
-then 
-    echo "Pass agent names"
-    echo "Example:"
-    echo "  sh scripts/run.sh baseline baseline"
+
+if [ -z $LUXAI_ROOT_PATH ]
+then
+    echo "LUXAI_ROOT_PATH is not specified!"
+    echo "Run: export LUXAI_ROOT_PATH=<path to project>"
+    echo "For example: export LUXAI_ROOT_PATH=/home/artemveshkin/dev/luxai-s3"
 else
-    echo "Run $1 agent VS $2 agent"
+    if [ -z "$2" ]
+    then 
+        echo "Pass agent names"
+        echo "Example:"
+        echo "  sh scripts/run.sh baseline baseline"
+    else
+        echo "Run $1 agent VS $2 agent"
 
-    BASE_DIR="$HOME/dev/luxai-s3" # CHANGE IT!!!
-    SCRIPT_WORK_DIR="$BASE_DIR/runs/$1_vs_$2"
-    KIT_PATH="Lux-Design-S3/kits/python"
+        SCRIPT_WORK_DIR="$LUXAI_ROOT_PATH/runs/$1_vs_$2"
+        KIT_PATH="Lux-Design-S3/kits/python"
 
-    AGENT_1_PATH="$SCRIPT_WORK_DIR/agent_1"
-    AGENT_2_PATH="$SCRIPT_WORK_DIR/agent_2"
+        AGENT_1_PATH="$SCRIPT_WORK_DIR/agent_1"
+        AGENT_2_PATH="$SCRIPT_WORK_DIR/agent_2"
 
-    rm -rf $SCRIPT_WORK_DIR && mkdir -p $SCRIPT_WORK_DIR
-    mkdir $AGENT_1_PATH && cp -r "$BASE_DIR/agents/$1/." $AGENT_1_PATH
-    mkdir $AGENT_2_PATH && cp -r "$BASE_DIR/agents/$2/." $AGENT_2_PATH
+        rm -rf $SCRIPT_WORK_DIR && mkdir -p $SCRIPT_WORK_DIR
+        mkdir $AGENT_1_PATH && cp -r "$LUXAI_ROOT_PATH/agents/$1/." $AGENT_1_PATH
+        mkdir $AGENT_2_PATH && cp -r "$LUXAI_ROOT_PATH/agents/$2/." $AGENT_2_PATH
 
-    cp "$BASE_DIR/$KIT_PATH/main.py" $AGENT_1_PATH
-    cp "$BASE_DIR/$KIT_PATH/main.py" $AGENT_2_PATH
+        cp "$LUXAI_ROOT_PATH/$KIT_PATH/main.py" $AGENT_1_PATH
+        cp "$LUXAI_ROOT_PATH/$KIT_PATH/main.py" $AGENT_2_PATH
 
-    cp -rp "$BASE_DIR/$KIT_PATH/lux" $AGENT_1_PATH
-    cp -rp "$BASE_DIR/$KIT_PATH/lux" $AGENT_2_PATH
+        cp -rp "$LUXAI_ROOT_PATH/$KIT_PATH/lux" $AGENT_1_PATH
+        cp -rp "$LUXAI_ROOT_PATH/$KIT_PATH/lux" $AGENT_2_PATH
 
-    luxai-s3 "$AGENT_1_PATH/main.py" "$AGENT_2_PATH/main.py" \
-        --output="$SCRIPT_WORK_DIR/replay.html"
+        luxai-s3 "$AGENT_1_PATH/main.py" "$AGENT_2_PATH/main.py" \
+            --output="$SCRIPT_WORK_DIR/replay.html"
+    fi
 fi
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then 
-    echo "Pass agent name"
-    echo "Example:"
-    echo "  sh scripts/submit.sh baseline"
+if [ -z $LUXAI_ROOT_PATH ]
+then
+    echo "LUXAI_ROOT_PATH is not specified!"
+    echo "Run: export LUXAI_ROOT_PATH=<path to project>"
+    echo "For example: export LUXAI_ROOT_PATH=/home/artemveshkin/dev/luxai-s3"
 else
-    echo "Creating submit for $1 agent"
+    if [ -z "$1" ]
+    then 
+        echo "Pass agent name"
+        echo "Example:"
+        echo "  sh scripts/submit.sh baseline"
+    else
+        echo "Creating submit for $1 agent"
+        SCRIPT_WORK_DIR="$LUXAI_ROOT_PATH/submit"
+        KIT_PATH="Lux-Design-S3/kits/python"
 
-    BASE_DIR="$HOME/dev/luxai-s3" # CHANGE IT!!!
-    SCRIPT_WORK_DIR="$BASE_DIR/submit"
-    KIT_PATH="Lux-Design-S3/kits/python"
+        rm -rf $SCRIPT_WORK_DIR && mkdir -p $SCRIPT_WORK_DIR
+        cp -r "$LUXAI_ROOT_PATH/agents/$1/." $SCRIPT_WORK_DIR
 
-    rm -rf $SCRIPT_WORK_DIR && mkdir -p $SCRIPT_WORK_DIR
-    cp -r "$BASE_DIR/agents/$1/." $SCRIPT_WORK_DIR
+        cp "$LUXAI_ROOT_PATH/$KIT_PATH/main.py" $SCRIPT_WORK_DIR
+        cp -rp "$LUXAI_ROOT_PATH/$KIT_PATH/lux" $SCRIPT_WORK_DIR
 
-    cp "$BASE_DIR/$KIT_PATH/main.py" $SCRIPT_WORK_DIR
-    cp -rp "$BASE_DIR/$KIT_PATH/lux" $SCRIPT_WORK_DIR
-
-    cd $SCRIPT_WORK_DIR && tar -czvf "$BASE_DIR/submit.tar.gz" *
-    rm -rf $SCRIPT_WORK_DIR
+        cd $SCRIPT_WORK_DIR && tar -czvf "$LUXAI_ROOT_PATH/submit.tar.gz" *
+        rm -rf $SCRIPT_WORK_DIR
+    fi
 fi
