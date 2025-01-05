93aec9149c8977a920b8320e87d3ae231066e18a
artemveshkin <artemveshkin@gmail.com>
2025-01-05 18:20:39 +0300

    log_energy script

--- scripts/log_energy.py
+++ scripts/log_energy.py
@@ -18,8 +18,10 @@ import json
 class Args:
     agent: str = "energy_logger"
     """Agent 1 name"""
-    n_runs: int = 1
+    n_runs: int = 100
     """How much matches to play"""
+    test_size: float = 0.2
+    """Test size"""
 
 
 def process_logs(raw_logs_dir, output_dir, game_idx):
@@ -68,6 +70,16 @@ def process_logs(raw_logs_dir, output_dir, game_idx):
                 pickle.dump({'x': x, 'gt': gt}, fh, protocol=pickle.HIGHEST_PROTOCOL)
 
 
+def train_test_split(parsed_logs_dir, train_dir, test_dir, n_test_games):
+    for parsed_log in tqdm(os.listdir(parsed_logs_dir)):
+        game_idx = int(parsed_log.split('_')[0])
+        is_train = True
+        if game_idx >= n_test_games:
+            is_train = False
+
+        os.system(f'mv {parsed_logs_dir / parsed_log} {(train_dir if is_train else test_dir)}/')
+
+
 def main():
     args = tyro.cli(Args)
     agent_name = args.agent
@@ -96,6 +108,25 @@ def main():
         time.sleep(0.2)
 
         process_logs(SCRIPT_DIR, SCRIPT_DIR / 'processed_logs', run_idx)
+
+    os.makedirs(SCRIPT_DIR / 'train')
+    os.makedirs(SCRIPT_DIR / 'test')
+
+    test_game_idx = int(n_runs * (1 - args.test_size))
+    train_test_split(
+        SCRIPT_DIR / 'processed_logs',
+        SCRIPT_DIR / 'train',
+        SCRIPT_DIR / 'test',
+        test_game_idx
+    )
+
+    os.system(f'rm -rf {SCRIPT_DIR}/processed_logs')
+    os.system(f'rm -rf {SCRIPT_DIR}/energy_logger')
+    os.system(f'rm -rf {SCRIPT_DIR}/out.json')
+    os.system(f'rm -rf {SCRIPT_DIR}/player_0_energy_fields.npy')
+    os.system(f'rm -rf {SCRIPT_DIR}/player_0_prev_energy_fields.npy')
+    os.system(f'rm -rf {SCRIPT_DIR}/player_1_energy_fields.npy')
+    os.system(f'rm -rf {SCRIPT_DIR}/player_1_prev_energy_fields.npy')
 
 
 if __name__ == "__main__":
