ccec76cf73414b4959a08c007e233b2e116d59b1
Artemii Veshkin <artem.veshkin@gmail.com>
2025-01-14 03:11:05 +0300

    genergy_predictor_1 agent

---
+++ agents/energy_predictor_1/agent.py
@@ -0,0 +1,341 @@
+import numpy as np
+from sys import stderr
+
+from base import (
+    Global,
+    get_match_step,
+    is_team_sector,
+)
+from space import Space
+from fleet import Fleet
+from node import Node
+from action_type import ActionType
+from pathfinding import (
+    astar,
+    find_closest_target,
+    nearby_positions,
+    create_weights,
+    estimate_energy_cost,
+    path_to_actions,
+    manhattan_distance,
+)
+from debug import (
+    show_map,
+    show_energy_field,
+    show_exploration_map,
+    show_predicted_energy_field
+)
+
+
+class Agent:
+    def __init__(self, player: str, env_cfg) -> None:
+        self.player = player
+        self.opp_player = "player_1" if self.player == "player_0" else "player_0"
+        self.team_id = 0 if self.player == "player_0" else 1
+        self.opp_team_id = 1 if self.team_id == 0 else 0
+        self.env_cfg = env_cfg
+
+        Global.MAX_UNITS = env_cfg["max_units"]
+        Global.UNIT_MOVE_COST = env_cfg["unit_move_cost"]
+        Global.UNIT_SAP_COST = env_cfg["unit_sap_cost"]
+        Global.UNIT_SAP_RANGE = env_cfg["unit_sap_range"]
+        Global.UNIT_SENSOR_RANGE = env_cfg["unit_sensor_range"]
+
+        self.space = Space()
+        self.fleet = Fleet(self.team_id)
+        self.opp_fleet = Fleet(self.opp_team_id)
+
+
+    def act(self, step: int, obs, remainingOverageTime: int = 60):
+        match_step = get_match_step(step)
+
+        # print(f"start step={match_step}({step})", file=stderr)
+
+        if match_step == 0:
+            # nothing to do here at the beginning of the match
+            # just need to clean up some of the garbage that was left after the previous match
+            self.fleet.clear()
+            self.opp_fleet.clear()
+            self.space.clear()
+            self.space.move_obstacles(step)
+            return self.create_actions_array()
+
+        points = int(obs["team_points"][self.team_id])
+
+        # how many points did we score in the last step
+        reward = max(0, points - self.fleet.points)
+
+        self.space.update(step, obs, self.team_id, reward)
+        self.fleet.update(obs, self.space)
+        self.opp_fleet.update(obs, self.space)
+
+        # if step < 5:
+        #     print(f"start step={match_step}({step})", file=stderr)
+        #     self.show_visible_energy_field()
+        #     self.show_predicted_energy_field()
+
+        self.find_relics()
+        self.find_rewards()
+        self.harvest()
+
+        # for ship in self.fleet:
+        #     print(ship, ship.task, ship.target, ship.action, file=stderr)
+
+        return self.create_actions_array()
+
+    def create_actions_array(self):
+        ships = self.fleet.ships
+        actions = np.zeros((len(ships), 3), dtype=int)
+
+        for i, ship in enumerate(ships):
+            if ship.action is not None:
+                actions[i] = ship.action, 0, 0
+
+        return actions
+
+    def find_relics(self):
+        if Global.ALL_RELICS_FOUND:
+            for ship in self.fleet:
+                if ship.task == "find_relics":
+                    ship.task = None
+                    ship.target = None
+            return
+
+        targets = set()
+        for node in self.space:
+            if not node.explored_for_relic:
+                # We will only find relics in our part of the map
+                # because relics are symmetrical.
+                if is_team_sector(self.fleet.team_id, *node.coordinates):
+                    targets.add(node.coordinates)
+
+        def set_task(ship):
+            if ship.task and ship.task != "find_relics":
+                return False
+
+            if ship.energy < Global.UNIT_MOVE_COST:
+                return False
+
+            target, _ = find_closest_target(ship.coordinates, targets)
+            if not target:
+                return False
+
+            path = astar(create_weights(self.space), ship.coordinates, target)
+            energy = estimate_energy_cost(self.space, path)
+            actions = path_to_actions(path)
+            if actions and ship.energy >= energy:
+                ship.task = "find_relics"
+                ship.target = self.space.get_node(*target)
+                ship.action = actions[0]
+
+                for x, y in path:
+                    for xy in nearby_positions(x, y, Global.UNIT_SENSOR_RANGE):
+                        if xy in targets:
+                            targets.remove(xy)
+
+                return True
+
+            return False
+
+        for ship in self.fleet:
+            if set_task(ship):
+                continue
+
+            if ship.task == "find_relics":
+                ship.task = None
+                ship.target = None
+
+    def find_rewards(self):
+        if Global.ALL_REWARDS_FOUND:
+            for ship in self.fleet:
+                if ship.task == "find_rewards":
+                    ship.task = None
+                    ship.target = None
+            return
+
+        unexplored_relics = self.get_unexplored_relics()
+
+        relic_node_to_ship = {}
+        for ship in self.fleet:
+            if ship.task == "find_rewards":
+                if ship.target is None:
+                    ship.task = None
+                    continue
+
+                if (
+                    ship.target in unexplored_relics
+                    and ship.energy > Global.UNIT_MOVE_COST * 5
+                ):
+                    relic_node_to_ship[ship.target] = ship
+                else:
+                    ship.task = None
+                    ship.target = None
+
+        for relic in unexplored_relics:
+            if relic not in relic_node_to_ship:
+
+                # find the closest ship to the relic node
+                min_distance, closes_ship = float("inf"), None
+                for ship in self.fleet:
+                    if ship.task and ship.task != "find_rewards":
+                        continue
+
+                    if ship.energy < Global.UNIT_MOVE_COST * 5:
+                        continue
+
+                    distance = manhattan_distance(ship.coordinates, relic.coordinates)
+                    if distance < min_distance:
+                        min_distance, closes_ship = distance, ship
+
+                if closes_ship:
+                    relic_node_to_ship[relic] = closes_ship
+
+        def set_task(ship, relic_node, can_pause):
+            targets = []
+            for x, y in nearby_positions(
+                *relic_node.coordinates, Global.RELIC_REWARD_RANGE
+            ):
+                node = self.space.get_node(x, y)
+                if not node.explored_for_reward and node.is_walkable:
+                    targets.append((x, y))
+
+            target, _ = find_closest_target(ship.coordinates, targets)
+
+            if target == ship.coordinates and not can_pause:
+                target, _ = find_closest_target(
+                    ship.coordinates,
+                    [
+                        n.coordinates
+                        for n in self.space
+                        if n.explored_for_reward and n.is_walkable
+                    ],
+                )
+
+            if not target:
+                return
+
+            path = astar(create_weights(self.space), ship.coordinates, target)
+            energy = estimate_energy_cost(self.space, path)
+            actions = path_to_actions(path)
+
+            if actions and ship.energy >= energy:
+                ship.task = "find_rewards"
+                ship.target = self.space.get_node(*target)
+                ship.action = actions[0]
+
+        can_pause = True
+        for n, s in sorted(
+            list(relic_node_to_ship.items()), key=lambda _: _[1].unit_id
+        ):
+            if set_task(s, n, can_pause):
+                if s.target == s.node:
+                    # If one ship is stationary, we will move all the other ships.
+                    # This will help generate more useful data in Global.REWARD_RESULTS.
+                    can_pause = False
+            else:
+                if s.task == "find_rewards":
+                    s.task = None
+                    s.target = None
+
+    def get_unexplored_relics(self) -> list[Node]:
+        relic_nodes = []
+        for relic_node in self.space.relic_nodes:
+            if not is_team_sector(self.team_id, *relic_node.coordinates):
+                continue
+
+            explored = True
+            for x, y in nearby_positions(
+                *relic_node.coordinates, Global.RELIC_REWARD_RANGE
+            ):
+                node = self.space.get_node(x, y)
+                if not node.explored_for_reward and node.is_walkable:
+                    explored = False
+                    break
+
+            if explored:
+                continue
+
+            relic_nodes.append(relic_node)
+
+        return relic_nodes
+
+    def harvest(self):
+
+        def set_task(ship, target_node):
+            if ship.node == target_node:
+                ship.task = "harvest"
+                ship.target = target_node
+                ship.action = ActionType.center
+                return True
+
+            path = astar(
+                create_weights(self.space),
+                start=ship.coordinates,
+                goal=target_node.coordinates,
+            )
+            energy = estimate_energy_cost(self.space, path)
+            actions = path_to_actions(path)
+
+            if not actions or ship.energy < energy:
+                return False
+
+            ship.task = "harvest"
+            ship.target = target_node
+            ship.action = actions[0]
+            return True
+
+        booked_nodes = set()
+        for ship in self.fleet:
+            if ship.task == "harvest":
+                if ship.target is None:
+                    ship.task = None
+                    continue
+
+                if set_task(ship, ship.target):
+                    booked_nodes.add(ship.target)
+                else:
+                    ship.task = None
+                    ship.target = None
+
+        targets = set()
+        for n in self.space.reward_nodes:
+            if n.is_walkable and n not in booked_nodes:
+                targets.add(n.coordinates)
+        if not targets:
+            return
+
+        for ship in self.fleet:
+            if ship.task:
+                continue
+
+            target, _ = find_closest_target(ship.coordinates, targets)
+
+            if target and set_task(ship, self.space.get_node(*target)):
+                targets.remove(target)
+            else:
+                ship.task = None
+                ship.target = None
+
+    def show_visible_energy_field(self):
+        print("Visible energy field:", file=stderr)
+        show_energy_field(self.space)
+
+    def show_predicted_energy_field(self):
+        print("Predicted energy field:", file=stderr)
+        show_predicted_energy_field(self.space)
+
+    def show_explored_energy_field(self):
+        print("Explored energy field:", file=stderr)
+        show_energy_field(self.space, only_visible=False)
+
+    def show_visible_map(self):
+        print("Visible map:", file=stderr)
+        show_map(self.space, self.fleet, self.opp_fleet)
+
+    def show_explored_map(self):
+        print("Explored map:", file=stderr)
+        show_map(self.space, self.fleet, only_visible=False)
+
+    def show_exploration_map(self):
+        print("Exploration map:", file=stderr)
+        show_exploration_map(self.space)
---
+++ agents/energy_predictor_1/energy_predictor.py
@@ -0,0 +1,53 @@
+import torch
+import numpy as np
+from base import SPACE_SIZE
+from sys import stderr
+from scipy.ndimage import zoom
+
+
+class EnergyPredictor:
+    def __init__(self):
+        self.energy_predictor = torch.jit.load('energy_predictor.pt')
+        self.energy_predictor.eval()
+        print("Loaded energy predictor", file=stderr)
+
+        # historical data
+        self.prev_energy_fields = [np.zeros((SPACE_SIZE, SPACE_SIZE, 2))] * 3
+
+    
+    def update_prev_energy_fields(self, space):
+        self.prev_energy_fields.append(space.get_energy_field())
+
+    
+    def predict_hidden_energy(self, space):
+        energy_field = space.get_energy_field()
+        prev_energy_fields = np.array(self.prev_energy_fields[-3:])
+
+        fields = np.stack([
+            energy_field[:, :, 0],
+            prev_energy_fields[0, :, :, 0],
+            prev_energy_fields[1, :, :, 0],
+            prev_energy_fields[2, :, :, 0],
+        ])
+        fields = (fields + 20.) / 40.
+
+        masks = np.stack([
+            energy_field[:, :, 1],
+            prev_energy_fields[0, :, :, 1],
+            prev_energy_fields[1, :, :, 1],
+            prev_energy_fields[2, :, :, 1],
+        ])
+
+        x = np.concatenate((fields, masks))
+        upsampled_x = np.zeros((8, 48, 48))
+        for i in range(x.shape[0]):
+            upsampled_x[i, :, :] = zoom(x[i, :, :], 2, order=0)
+
+        model_in = torch.unsqueeze(torch.Tensor(upsampled_x), 0)
+
+        predicted_energy = self.energy_predictor(model_in).detach().numpy()[0, 0, :, :]
+        predicted_energy = predicted_energy * 40. - 20.
+
+        for node in space:
+            x, y = node.coordinates
+            node.predicted_energy = node.energy if node.energy is not None else predicted_energy[x, y]
