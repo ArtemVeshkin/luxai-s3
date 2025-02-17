eda4013c89ac4d19da99882a76a1963ea075ee00
artemveshkin <artemveshkin@gmail.com>
2025-02-17 05:23:48 +0300

    delete test script

--- agents/state_logger/test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from pathlib import Path
-
-
-states_dir = Path('/Users/artemveshkin/dev/luxai-s3/state_logs/processed_states')
-with open(states_dir / '1_1.pickle', 'rb') as fh:
-    logged = pickle.load(fh)
-
-print(logged['state']['space'][''])
